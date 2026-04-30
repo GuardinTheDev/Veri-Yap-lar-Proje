976c1de592893edf37ee5f299e6090a008cc9c96
Zeynep-zer <zeynep@berkeltd.com>
2026-04-30 22:21:14 +0300

    Add files via upload

---
+++ backend_core_graph/Hashtable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend_core_graph
+{
+    internal class Hashtable
+    {
+        Durak[] Durak_dizisi = new Durak[1009];
+
+        int Hashfonk(int id)
+        {
+            return id % 1009;
+        }
+
+        public void Durak_Ekle(Durak d)
+        {
+            Durak_dizisi[Hashfonk(d.ID)] = d;
+        }
+
+        public Durak Durak_Getir(int id)
+        {
+            return Durak_dizisi[Hashfonk(id)];
+        }
+
+
+
+    }
+}
