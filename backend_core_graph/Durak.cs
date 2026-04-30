a7e2c5ab10fe5533955e9398090e1025b89ac3ca
Zeynep-zer <zeynep@berkeltd.com>
2026-04-30 11:20:21 +0300

    Add project files.

---
+++ backend_core_graph/Durak.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend_core_graph
+{
+    internal class Durak
+    {
+        public int ID { get; set; }
+        public double X { get; set; }
+        public double Y {  get; set; }
+        public string Ad { get; set; }
+
+        public Durak(int id, double x, double y,string ad) {
+            ID = id;
+            X = x;
+            Y = y;
+            Ad = ad;
+        }
+    }
+}
---
+++ backend_core_graph/Hat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend_core_graph
+{
+    internal class Hat
+    {
+        public string HatAd {  get; set; }
+        public double Mesafe { get; set; }
+        public double Sure { get; set; }
+        public Durak Baslangic { get; set; }
+        public Durak Hedef {  get; set; }
+
+        public Hat(string hatad,double mesafe,double sure,Durak baslangic, Durak hedef )
+        {
+            HatAd = hatad;
+            Mesafe = mesafe;
+            Sure = sure;
+            Baslangic = baslangic;
+            Hedef = hedef;
+        }
+
+       
+    }
+}
