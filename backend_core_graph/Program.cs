13f89b7249505e36bac7a2e1b604d5c53f5c7491
cryobyte-py <cryobyte_0@protonmail.com>
2026-05-11 19:57:23 +0300

    updated the .NET version

--- backend_core_graph/Program.cs
+++ backend_core_graph/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace backend_core_graph
+﻿namespace backend_core_graph
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
