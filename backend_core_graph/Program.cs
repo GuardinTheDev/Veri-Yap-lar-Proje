fd7d7886aa7eaa6e6fb518c795d95fe3ce2b8254
= <=>
2026-05-18 14:28:52 +0300

    astar icin birkac implementation (ileride degistirilecek)

--- backend_core_graph/Program.cs
+++ backend_core_graph/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace backend_core_graph
+﻿using System.Collections.Generic;
+using System.IO;
+namespace backend_core_graph
 {
     internal class Program
-    {
+    {   
+        public void LoadJSON()
+        {
+            using (StreamReader reader = new StreamReader("../python_scripts/test_sehir.json"))
+            {
+                string json = reader.ReadToEnd();
+                
+            }
+        }
         static void Main(string[] args)
         {
-            
+            Multigraph multigraph = new Multigraph();
+
         }
     }
 }
