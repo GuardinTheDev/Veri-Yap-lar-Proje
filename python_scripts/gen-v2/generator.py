8c0e3485bf982ca69ecb397d6b8d3abddde45f6d
cryobyte-py <cryobyte_0@protonmail.com>
2026-05-25 19:52:28 +0300

    generator v2: completed coordinate generation with random names and adjustable minimum distances

--- python_scripts/gen-v2/generator.py
+++ python_scripts/gen-v2/generator.py
@@ -1,8 +1,13 @@
 from faker import Faker
 import json
 import os
+import math
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+from itertools import product
 
-
+fakegen = Faker()
         
 class WeightedTransitGraph():
     AutoIntegrityCheck = False
@@ -65,24 +70,42 @@ class WeightedTransitGraph():
         def convertLineNodeToTransitNode(self, node):
             pass
         
-        def checkIntegrity(self):
+        def checkIntegrity(self): # check if the linked list is valid
             if len(self.nodeList) == 0: 
                 print("List empty")
                 return
+            
             visitedNodes = []
-            def traverse(self):
-                iterations = 0
-                node = self.nodeList[0]
-                while True:
-                    visitedNodes.append(node)
-                    node = node.nextNode
-                    iterations += 1
+            iterations = 0
+            node: WeightedTransitGraph.TransitLine.LineStopNode
+            node = self.nodeList[0]
+            iterations += 1
+            visitedNodes.append(node)
+            node = node.nextNode
 
-                    if node == self.nodeList[0]:
-                        print("reached initial node")
-                        self.lineType = self.__class__.TransitLineTypes.CircularLine()
+            # traversal function
+            while True:
+                if node is None:
+                    print("End of list")
+                    self.lineType = WeightedTransitGraph.TransitLine.TransitLineTypes.LinearLine
+                    break
 
+                if node == self.nodeList[0]:
+                    print("reached initial node")
+                    self.lineType = WeightedTransitGraph.TransitLine.TransitLineTypes.CircularLine
+                    break
                     
+                if node in visitedNodes:
+                    print("there is a loop inside")
+                    self.lineType = WeightedTransitGraph.TransitLine.TransitLineTypes.InvalidLine
+                    break
+                visitedNodes.append(node)
+                iterations += 1
+                node = node.nextNode
+
+            if iterations != len(self.nodeList):
+                self.lineType = WeightedTransitGraph.TransitLine.TransitLineTypes.InvalidLine
+                print("invalid list")
 
 
     class TransitStopNode():
@@ -114,3 +137,93 @@ class WeightedTransitGraph():
         for i in self.nodeList:
             if i.name == name:
                 return(i)
+
+    class RandomGenerator():
+        def __init__(self, coordRange: tuple):
+            # generate a square map
+            if len(coordRange) != 2:
+                raise(ValueError)
+            for i in coordRange:
+                if type(i) is not int:
+                    raise(TypeError)
+            self.coordRange = coordRange
+            self.stopList = []
+        
+
+        def generateStopCoordinates(self, faker:Faker, stopCount:int, minDist=0):
+
+            @staticmethod
+            def euclideanDistance(a,b):
+                return math.sqrt(math.pow(a[0]-b[0],2) + math.pow(a[1]-b[1],2))
+
+            @staticmethod
+            def isFarEnough(minDist, list, coord:tuple):
+                for i in list:
+                    dist = euclideanDistance(i, coord)
+                    if dist < minDist:
+                        return False
+                return True
+
+
+            lastIndex = len(self.stopList)
+
+            # generate all unique tuple possibilities within range and sample stopCount of them
+            #coordList = random.sample(list(product(range(self.coordRange[0], self.coordRange[1]), repeat=2)), k=stopCount)
+
+            # manual sampling:
+            allPossibleCoordinates = list(product(range(self.coordRange[0], self.coordRange[1]), repeat=2))
+            #print(allPossibleCoordinates)
+            print(len(allPossibleCoordinates))
+            if stopCount > len(allPossibleCoordinates): raise(ValueError) 
+            # alternatively, use (abs(self.coordRange[1]-self.coordRange[0]))^2 which would return the same value
+            # raise an exception if asked to sample more stops than possible
+
+            coordList = []
+            while True:
+                if len(allPossibleCoordinates)==0:
+                    raise(ValueError)
+                if len(coordList) == stopCount:
+                    break
+                v: int
+                max = len(allPossibleCoordinates)-1
+                v = random.randint(0,max)
+                print(v)
+                temp = allPossibleCoordinates.pop(v)
+                #print(temp)
+                if isFarEnough(minDist, coordList, temp):
+                    coordList.append(temp)
+                    print(temp)
+                
+
+            for i,v in enumerate(coordList):
+                newStop = {
+                    "id": i+lastIndex,
+                    "name": faker.street_address(),
+                    "x": v[0],
+                    "y": v[1],
+                    "lines": None
+                }
+                self.stopList.append(newStop)
+        
+        def plotStops(self):
+            fig = plt.figure()
+            plot1 = fig.add_subplot(1,1,1)
+            x = []
+            y = []
+            txt = []
+            for stop in self.stopList:
+                x.append(stop["x"])
+                y.append(stop["y"])
+                txt.append("ID:"+str(stop["id"]))
+            x = np.array(x)
+            y = np.array(y)
+            plot1.scatter(x,y)
+            for i, v in enumerate(txt):
+                plot1.annotate(v, (x[i], y[i]))
+            plt.show()
+
+
+gen = WeightedTransitGraph.RandomGenerator((0,200))
+gen.generateStopCoordinates(fakegen, 20, 10)
+print(gen.stopList)
+gen.plotStops()
